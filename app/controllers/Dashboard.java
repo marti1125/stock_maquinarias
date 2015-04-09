bccccb29036eed4ee19c4dec5354bbee497fdd76
Willy Aguirre <marti1125@gmail.com>
2015-04-09 15:18:55 -0500

    lista la informacion de los tramite

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import dtos.PrincipalDto;
+
+import models.Principal;
+import models.StockMunicipalidad;
 import play.mvc.Controller;
 import play.mvc.With;
 
@@ -8,7 +16,31 @@ import play.mvc.With;
 public class Dashboard extends Controller {
 	
 	public static void index() {
-		render();
+		
+		List<Principal> principales = Principal.findAll();
+		
+		List<PrincipalDto> datos = new ArrayList<PrincipalDto>();
+		
+		for(Principal p : principales){
+			PrincipalDto principal = new PrincipalDto();
+			principal.dni = p.dni;
+			principal.ciudadano = p.ciudadano;
+			principal.direccionCiudadano = p.direccionCiudadano;
+			principal.telefono = p.telefono;
+			principal.ruc = p.ruc;	
+			principal.empresa = p.empresa;	  	
+			principal.direccionEmpresa = p.direccionEmpresa;   	
+			principal.fechaInicio = p.fechaInicio; 
+			principal.fechaFin = p.fechaFin;
+			StockMunicipalidad nombre = new StockMunicipalidad().findById(p.stockMunicipalidad);
+			principal.stockMunicipalidad = 	nombre.tipoMaquinaria.nombre;     	
+			principal.costoTotal = p.costoTotal;
+			principal.municipalidad = p.municipalidad;
+			principal.fechaCreacion = p.fechaCreacion;
+			datos.add(principal);
+		}
+		
+		render(datos);
 	}
 	
 }
---
+++ app/dtos/PrincipalDto.java
@@ -0,0 +1,31 @@
+package dtos;
+
+import java.util.Date;
+
+public class PrincipalDto {
+	public String dni;
+	
+	public String ciudadano;
+	
+	public String direccionCiudadano;
+	
+	public String telefono;
+	
+	public String ruc;
+	
+	public String empresa;
+	
+	public String direccionEmpresa;
+	
+	public String fechaInicio;
+	
+	public String fechaFin;
+	
+	public String stockMunicipalidad;
+	
+	public String costoTotal;
+	
+	public String municipalidad;
+	
+	public Date fechaCreacion;
+}
